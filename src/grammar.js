6a2f40c471a74e19bd647be2001dfc65483ce2a7
Dennis Dunn <ansofive@gmail.com>
2022-02-09 12:22:26 -0700

    Adding scaffolding.

---
+++ src/grammar.js
@@ -0,0 +1,5 @@
+const { choice, many, sequence, optional, anyOfChar, whitespace, number, map, str, between } = require('@dennisdunn/tiny-parse');
+
+module.exports = {
+    start: null // the grammars starting rule
+}
---
+++ tests/grammar.test.js
@@ -0,0 +1,20 @@
+const { context } = require('@dennisdunn/tiny-parse');
+const { G } = require('../src/grammar');
+
+const log = obj => {
+    console.log(`${expect.getState().currentTestName} => ${obj.result}`);
+}
+
+const run = str => {
+    const parser = G.start;
+    const result = parser(context(str));
+    log(result);
+}
+
+test('parse 1 + 2', () => {
+    run('1 + 2');
+})
+
+test('parse (1 + 2)* (1 - 2 * 3)', () => {
+    run('(1 + 2)* (1 - 2 * 3)');
+})
