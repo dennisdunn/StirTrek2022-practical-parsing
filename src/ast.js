5f072cb8ad4925a5798944db9401b4abf4605c7f
Dennis Dunn <ansofive@gmail.com>
2022-02-09 13:00:13 -0700

    as-abstract-syntax-tree

---
+++ src/ast.js
@@ -0,0 +1,95 @@
+class AST {
+    static open_lparen = () => new AST_OpenParen();
+    static close_paren = () => new AST_CloseParen();
+    static sum_op = value => new AST_BinaryOp(value);
+    static prod_op = value => new AST_BinaryOp(value);
+    static number = value => new AST_Number(value);
+
+    static head_handler = value => {
+        if (Array.isArray(value)) {
+            switch (value.length) {
+                case 1:
+                    return value[0];
+                case 2:
+                    value[1].left = value[0];
+                    break;
+            }
+            return value[1];
+        }
+        return value;
+    };
+
+    static tail_handler = value => {
+        if (Array.isArray(value)) {
+            switch (value.length) {
+                case 1:
+                    return value[0];
+                case 2:
+                    value[0].right = value[1];
+                    break;
+                case 3:
+                    value[2].left = value[1];
+                    value[0].right = value[2];
+                    break;
+            }
+            return value[0];
+        }
+        return value;
+    };
+}
+
+class AST_Node {
+    constructor(type, value = null) {
+        this.type = type.toUpperCase();
+        if (value) this.value = value;
+    }
+
+    eval() { }
+}
+
+class AST_OpenParen extends AST_Node {
+    constructor() {
+        super('open_paren');
+    }
+}
+
+class AST_CloseParen extends AST_Node {
+    constructor() {
+        super('close_paren');
+    }
+}
+
+class AST_BinaryOp extends AST_Node {
+    constructor(value) {
+        super('binary_op', value);
+    }
+    
+    eval() {
+        const operandA = this.left.eval();
+        const operandB = this.right.eval();
+        switch (this.value) {
+            case '+':
+                return operandA + operandB;
+            case '-':
+                return operandA - operandB;
+            case '*':
+                return operandA * operandB;
+            case '/':
+                return operandA / operandB;
+        }
+    }
+}
+
+class AST_Number extends AST_Node {
+    constructor(value) {
+        super('number', value);
+    }
+
+    eval(){
+        return parseFloat(this.value);
+    }
+}
+
+module.exports = {
+    AST
+}
--- src/grammar.js
+++ src/grammar.js
@@ -1,5 +1,49 @@
 const { choice, many, sequence, optional, anyOfChar, whitespace, number, map, str, between } = require('@dennisdunn/tiny-parse');
+const { AST } = require('./ast');
+
+// run a parser and remove any trailing whitespace
+const ignore_ws = parser => map(sequence(parser, optional(many(whitespace))), value => Array.isArray(value) ? value[0] : value);
+
+// summation operator
+const sop = map(ignore_ws(anyOfChar('+-')), AST.sum_op);
+
+// product operator
+const pop = map(ignore_ws(anyOfChar('*/')), AST.prod_op);
+
+// open paren
+const lpar = map(ignore_ws(str('(')), AST.open_lparen);
+
+// close paren
+const rpar = map(ignore_ws(str(')')), AST.close_paren);
+
+// numbers
+const number_ws = map(ignore_ws(number), AST.number);
+
+// expression (head)
+function E(ctx) {
+    return map(sequence(T, E1), AST.head_handler)(ctx);
+}
+
+// expression prime (tail)
+function E1(ctx) {
+    return map(optional(sequence(sop, T, E1)), AST.tail_handler)(ctx)
+}
+
+// term (head)
+function T(ctx) {
+    return map(sequence(F, T1), AST.head_handler)(ctx)
+}
+
+// term (tail)
+function T1(ctx) {
+    return map(optional(sequence(pop, F, T1)), AST.tail_handler)(ctx)
+}
+
+// factor
+function F(ctx) {
+    return choice(number_ws, between(lpar, E, rpar))(ctx);
+}
 
 module.exports = {
-    start: null // the grammars starting rule
+    start: E
 }
