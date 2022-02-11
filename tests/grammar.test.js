5f96b51c41089075734feeaf270157b0060dbfce
Dennis Dunn <ansofive@gmail.com>
2022-02-11 16:27:13 -0700

    Updated to tiny-parse 0.5.4. Using toStrictEqual in the tests.

--- tests/grammar.test.js
+++ tests/grammar.test.js
@@ -1,20 +1,14 @@
 const { Stream, flat } = require('@dennisdunn/tiny-parse');
 const G = require('../src/grammar');
 
-const log = obj => {
-    console.log(`${expect.getState().currentTestName} =>\n ${JSON.stringify(obj.result)}`);
-}
-
-const run = text => {
-    const parser = flat(G.start);
-    const result = parser(new Stream(text));
-    log(result);
-}
-
 test('parse 1 + 2', () => {
-    run('1 + 2');
+    const r = flat(G.start)(new Stream('1 + 2'));
+
+    expect(r).toStrictEqual([{ "type": "NUMBER", "value": "1" }, { "type": "SUM_OP", "value": "+" }, { "type": "NUMBER", "value": "2" }]);
 })
 
 test('parse (1 + 2)* (1 - 2 * 3)', () => {
-    run('(1 + 2)* (1 - 2 * 3)');
+    const r = flat(G.start)(new Stream('(1 + 2)* (1 - 2 * 3)'));
+
+    expect(r).toStrictEqual([{ "type": "OPEN_PAREN", "value": "(" }, { "type": "NUMBER", "value": "1" }, { "type": "SUM_OP", "value": "+" }, { "type": "NUMBER", "value": "2" }, { "type": "CLOSE_PAREN", "value": ")" }, { "type": "PROD_OP", "value": "*" }, { "type": "OPEN_PAREN", "value": "(" }, { "type": "NUMBER", "value": "1" }, { "type": "SUM_OP", "value": "-" }, { "type": "NUMBER", "value": "2" }, { "type": "PROD_OP", "value": "*" }, { "type": "NUMBER", "value": "3" }, { "type": "CLOSE_PAREN", "value": ")" }]);
 })
