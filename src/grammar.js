8c1f4b02d7ea50b433ff675bc6c93c79dcdbb756
Dennis Dunn <ansofive@gmail.com>
2022-02-11 17:00:36 -0700

    Removed some scaffolding bits. The weren't as handy as I thought they'd be.

--- src/grammar.js
+++
@@ -1,5 +0,0 @@
-const { choice, many, sequence, optional, anyOfChar, whitespace, number, map, str, between } = require('@dennisdunn/tiny-parse');
-
-module.exports = {
-    start: null // the grammars starting rule
-}
