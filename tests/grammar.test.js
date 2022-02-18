af6c44ece73b568575ab0b031aa675c0cb02a22f
Dennis Dunn <ansofive@gmail.com>
2022-02-17 21:51:25 -0700

    Refactor for consistency.

--- tests/grammar.test.js
+++ tests/grammar.test.js
@@ -2,19 +2,22 @@ const { Stream } = require('@dennisdunn/tiny-parse');
 const G = require('../src/grammar');
 
 test('evaluate 1 + 2', () => {
-    const r = G.start(new Stream('1 + 2')).eval();
+    const tree = G.start(new Stream('1 + 2'));
+    const r = tree.eval();
 
     expect(r).toBe(3);
 })
 
 test('evaluate (1 + 2)* (1 - 2 * 3)', () => {
-    const r = G.start(new Stream('(1 + 2)* (1 - 2 * 3)')).eval();
+    const tree = G.start(new Stream('(1 + 2)* (1 - 2 * 3)'));
+    const r = tree.eval();
 
     expect(r).toBe(-15);
 })
 
 test('evaluate 1 + 2 * (1 - 2 * 3)', () => {
-    const r = G.start(new Stream('1 + 2 * (1 - 2 * 3)')).eval();
+    const tree = G.start(new Stream('1 + 2 * (1 - 2 * 3)'));
+    const r = tree.eval();
 
     expect(r).toBe(-9);
 })
