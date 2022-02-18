2e53a39a56e3ef0408fcf84ef9ba7b8952f56ae8
Dennis Dunn <ansofive@gmail.com>
2022-02-17 21:27:51 -0700

    Refactor for consistency.

--- src/grammar.js
+++ src/grammar.js
@@ -1,4 +1,4 @@
-const { choice, many, sequence, optional, anyOfChar, whitespace, number, map, str} = require('@dennisdunn/tiny-parse');
+const { choice, many, sequence, optional, anyOfChar, whitespace, number: parseNumber, map, str } = require('@dennisdunn/tiny-parse');
 
 // run a parser and remove any trailing whitespace
 const ignore_ws = parser => map(sequence(parser, optional(many(whitespace))), value => Array.isArray(value) ? value[0] : value);
@@ -10,39 +10,39 @@ const sop = ignore_ws(anyOfChar('+-'));
 const pop = ignore_ws(anyOfChar('*/'));
 
 // open paren
-const lpar = ignore_ws(str('('));
+const open = ignore_ws(str('('));
 
 // close paren
-const rpar = ignore_ws(str(')'));
+const close = ignore_ws(str(')'));
 
 // numbers
-const number_ws = ignore_ws(number);
+const number = ignore_ws(parseNumber);
 
 // expression (head)
-function E(ctx) {
-    return sequence(T, E1)(ctx);
+function Expr(ctx) {
+    return sequence(Term, Expr_Prime)(ctx);
 }
 
 // expression' (tail)
-function E1(ctx) {
-    return optional(sequence(sop, T, E1))(ctx)
+function Expr_Prime(ctx) {
+    return optional(sequence(sop, Term, Expr_Prime))(ctx)
 }
 
 // term (head)
-function T(ctx) {
-    return sequence(F, T1)(ctx)
+function Term(ctx) {
+    return sequence(Factor, Term_Prime)(ctx)
 }
 
 // term' (tail)
-function T1(ctx) {
-    return optional(sequence(pop, F, T1))(ctx)
+function Term_Prime(ctx) {
+    return optional(sequence(pop, Factor, Term_Prime))(ctx)
 }
 
 // factor
-function F(ctx) {
-    return choice(number_ws, sequence(lpar, E, rpar))(ctx);
+function Factor(ctx) {
+    return choice(number, sequence(open, Expr, close))(ctx);
 }
 
 module.exports = {
-    start: E
+    start: Expr
 }
